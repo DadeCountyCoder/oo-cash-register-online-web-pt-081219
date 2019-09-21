2503d0d9a567e427edf7986fc554670914a2568f
Alfredo Diaz <mail4alfredo@gmail.com>
2019-09-21 17:49:10 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,41 +1,41 @@
-class CashRegister
+# class CashRegister
   
-  attr_accessor :with_discount, :total, :items, :last_transaction_amount
+#   attr_accessor :with_discount, :total, :items, :last_transaction_amount
   
-  def initialize(with_discount=0)
-    @with_discount = with_discount
-    @total = 0
-    @items= []
-  end
+#   def initialize(with_discount=0)
+#     @with_discount = with_discount
+#     @total = 0
+#     @items= []
+#   end
   
-  def discount
-    self.with_discount
-  end  
+#   def discount
+#     self.with_discount
+#   end  
   
-  # def instance(total)
-  #   @total=total
-  #   total=0
-  #end
+#   # def instance(total)
+#   #   @total=total
+#   #   total=0
+#   #end
   
-  def add_item(title, price, quantity=1)
-    #   # binding.pry
-    # @total +=price*quantity
-    #   quantity.times.do
-    #     items << title
-      self.total += price * quantity
-  end
+#   def add_item(title, price, quantity=1)
+#     #   # binding.pry
+#     # @total +=price*quantity
+#     #   quantity.times.do
+#     #     items << title
+#       self.total += price * quantity
+#   end
   
 
-  def apply_discount
-      # binding.pry
-      if discount > 0 && total
-        self.total = (self.total * ((100.0 - discount.to_f)/100)).to_i
-      else
-        self.total
-  end
-    end
+#   def apply_discount
+#       # binding.pry
+#       if discount > 0 && total
+#         self.total = (self.total * ((100.0 - discount.to_f)/100)).to_i
+#       else
+#         self.total
+#   end
+#     end
  
-end
+# end
 
 # class CashRegister
 
